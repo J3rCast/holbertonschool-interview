f0c6c0170d3de23318ea268cfa0444046cb52cad
J3rCast <jerson.castro.r@gmail.com>
2022-12-22 20:35:23 -0500

    task 1 added

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,63 +1,155 @@
 #include "binary_trees.h"
-#include <stdio.h>
-/**
- * brother_empty - checks if the brother node is empty
- * @root: parent node of the brothers nodes to check
- * Return: left if has empty nodes or right if left is full
- */
-heap_t **brother_empty(heap_t **root)
-{
-	if ((*root)->left->left == NULL || (*root)->left->right == NULL)
-		return (&(*root)->left);
-
-	if ((*root)->right->left == NULL || (*root)->right->right == NULL)
-		return (&(*root)->right);
-
-	return (&(*root)->left);
-}
 
 /**
  * heap_insert - inserts a value into a Max Binary Heap
- * @root: is a double pointer to the root node of the Heap
- * @value: is the value to store in the node to be inserted
- * Return:  pointer to the inserted node, or NULL on failure
+ * Max Binary Heap must be complete tree with max as root node
+ * @root: a double pointer to the root node of the Heap
+ * @value: the value to store in the inserted node
+ *
+ * Return: pointer to the newly inserted node, or NULL if failed
  */
+
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node = NULL;
-	int temp = 0;
+	size_t level = 0, height = 0;
+	heap_t *location = NULL, *new = NULL;
 
-	if (*root && value > (*root)->n)
-	{
-		temp = (*root)->n;
-		(*root)->n = value;
-		value = temp;
-	}
-
-	new_node = binary_tree_node(*root, value);
-	if (new_node == NULL)
+	if (root == NULL)
 		return (NULL);
-
+	/* if given pointer to empty max binary heap, */
+	/*   create new node and set root pointer to new node */
 	if (*root == NULL)
 	{
-		*root = new_node;
-		return (new_node);
+		new = binary_tree_node(*root, value);
+		*root = new;
+		return (new);
 	}
-
-	if ((*root)->left == NULL)
+	/* Determine height of max heap to move through level-by-level */
+	height = binary_tree_height(*root);
+	/* Move through heap using level-order travesal */
+	for (level = 0; level < height; level++)
 	{
-		heap_insert(&((*root)->left), value);
-		(*root)->left = new_node;
-		return (new_node);
+		/* Use find_location to find next opening in complete tree */
+		location = find_location(*root, level);
+		/* if the location is not NULL, break and */
+		/*   location will be used as node to insert off of */
+		if (location != NULL)
+			break;
 	}
-	else if ((*root)->right == NULL)
+	/* Create and insert node with location as parent */
+	new = binary_tree_node(location, value);
+	/* if location had no left child, set left to new node */
+	if (!location->left)
+		location->left = new;
+	/* Otherwise, set new node as location's right child */
+	else
+		location->right = new;
+	/* Keep swapping nodes until max binary heap is achieved */
+	while (new->parent && new->n > new->parent->n)
 	{
-		heap_insert(&((*root)->right), value);
-		(*root)->right = new_node;
-		return (new_node);
+		new = swap_child(root, new);
 	}
-	heap_insert(brother_empty(root), value);
-
-	return (new_node);
+	/* return newly inserted node */
+	return (new);
 }
 
+/**
+ * binary_tree_height - finds the height of completed binary tree
+ * since the tree is completed, talled on left side
+ * @root: pointer to the root node of the tree
+ *
+ * Return: returns the height of the overall tree
+ */
+size_t binary_tree_height(heap_t *root)
+{
+	if (root == NULL)
+		return (0);
+	/* Recursively call function, adding 1 for each layer found */
+	/* Using left child, since heap is a complete tree */
+	return (1 + binary_tree_height(root->left));
+}
+
+/**
+ * find_location - finds first location that does not have a right child
+ * the location may be a leaf or only have left child
+ * since level-order traversal, will find location to add for complete tree
+ * @root: pointer to the root node of the tree
+ * @level: keeps track of the current level in the tree
+ *
+ * Return: location to insert new node off of, or NULL if has two children
+ */
+heap_t *find_location(heap_t *root, size_t level)
+{
+	heap_t *location = NULL;
+
+	/* return NULL if no root pointer */
+	if (root == NULL)
+		return (NULL);
+	/* if first level and no right child, return root node to insert from */
+	/* check for right child because initially given a complete tree - */
+	/*   doesn't matter if left child, it's first spot with missing child */
+	if (level == 0 && root->right == NULL)
+		return (root);
+	/* if first level with both children, return NULL to continue */
+	else if (level == 0)
+		return (NULL);
+	/* recursively call function for next level with left child as root */
+	location = find_location(root->left, level - 1);
+	/* if recursive call found valid location, return that location */
+	if (location != NULL)
+		return (location);
+	/* recursively call function for next level with right child as root */
+	location = find_location(root->right, level - 1);
+	/* return valid location or NULL to continue */
+	return (location);
+}
+
+/**
+ * swap_child - swaps the new node with its parent
+ * @root: double pointer to the root node of the max binary heap
+ * @new: recently inserted node to swap
+ *
+ * Return: pointer to new node after switch
+ */
+heap_t *swap_child(heap_t **root, heap_t *new)
+{
+	int left = 0;
+	heap_t *temp = new->parent, *temp_r = temp->right, *temp_l = temp->left;
+
+	if (new->parent->left == new)
+		left = 1;
+	new->parent->right = new->right;
+	if (new->right)
+		new->right->parent = new->parent;
+	new->parent->left = new->left;
+	if (new->left)
+		new->left->parent = new->parent;
+	if (left)
+	{
+		new->right = temp_r;
+		if (temp_r)
+			temp_r->parent = new;
+	}
+	else
+	{
+		new->left = temp_l;
+		if (temp_l)
+			temp_l->parent = new;
+	}
+	new->parent = temp->parent;
+	if (temp->parent)
+	{
+		if (temp->parent->left == temp)
+			temp->parent->left = new;
+		else
+			temp->parent->right = new;
+	}
+	else
+		*root = new;
+	if (left)
+		new->left = temp;
+	else
+		new->right = temp;
+	temp->parent = new;
+	return (new);
+}
--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,8 +1,11 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
+/* INCLUDED LIBRARIES */
+#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+
+/* STRUCTS AND DEFINITIONS */
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -12,17 +15,35 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
+
+/* Binary Tree */
+typedef struct binary_tree_s binary_tree_t;
+
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
-void binary_tree_print(const binary_tree_t *);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-heap_t *heap_insert(heap_t **root, int value);
+/* FUNCTION PROTOTYPES */
 
-#endif /* _BINARY_TREES_H_ */
+/* given function to print binary tree */
+void binary_tree_print(const binary_tree_t *);
+
+/* function that creates a binary tree node */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* function that inserts a value into a Max Binary Heap */
+heap_t *heap_insert(heap_t **root, int value);
+/* subfunction to determine the height of the tree */
+size_t binary_tree_height(heap_t *root);
+/* subfunction to find location to insert at to maintain completeness */
+heap_t *find_location(heap_t *root, size_t level);
+/* subfunction to swap newly inserted node with parent for max heap order */
+heap_t *swap_child(heap_t **root, heap_t *new);
+
+#endif /* BINARY_TREES_H */
