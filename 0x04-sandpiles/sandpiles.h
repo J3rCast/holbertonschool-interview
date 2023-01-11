ad2b2279857a28b2e47a547e11a350181c55def0
J3rCast <jerson.castro.r@gmail.com>
2023-01-11 15:04:08 -0500

    task 0 added

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+/* IMPORTS */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* PROTOTYPES */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif
