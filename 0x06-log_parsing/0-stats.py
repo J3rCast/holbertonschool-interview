51ff2b35001a80f648260de7db9f1a89dd3d507a
J3rCast <jerson.castro.r@gmail.com>
2023-01-26 11:26:17 -0500

    pycodestyle fixes

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,9 +5,6 @@ from time import sleep
 from dateutil.parser import parse
 
 
-
-
-
 try:
     data = sys.stdin
     correct_method = "\"GET /projects/260 HTTP/1.1\""
@@ -17,13 +14,13 @@ try:
 
     for idx, i in enumerate(data):
         splited = i.split(" ")
-    
+
         ip = splited[0]
         date = splited[2].replace("[", "") + " " + splited[3].replace("]", "")
         method = splited[4] + " " + splited[5] + " " + splited[6]
         status = int(splited[7])
         size = int(splited[8])
-    
+
         try:
             ipaddress.IPv4Address(ip)
             parse(date)
@@ -36,15 +33,15 @@ try:
                     status_dict[status] = 1
             else:
                 break
-        
+
             final_size += size
-            
+
             if ((idx + 1) % 10 == 0 and idx != 0):
                 print("File size: {}".format(final_size))
                 for i in correct_status:
                     if (i in status_dict):
                         print("{}: {}".format(i, status_dict[i]))
-        except:
+        except Exception:
             break
 except KeyboardInterrupt:
     print("File size: {}".format(final_size))
