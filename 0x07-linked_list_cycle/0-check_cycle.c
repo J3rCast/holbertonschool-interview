a2c43d3b140bded78bb1188760aee561e09eec7f
J3rCast <jerson.castro.r@gmail.com>
2023-01-30 09:36:52 -0500

    task 0 added

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * check_cycle - check if a linked list has a cycle.
+ *
+ * @list: linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp = NULL;
+
+	temp = list;
+	if (temp == NULL || temp->next == NULL)
+		return (0);
+
+	temp = temp->next;
+
+	while (temp != NULL && temp != list)
+		temp = temp->next;
+
+	if (temp == NULL)
+		return (0);
+	if (temp == list)
+		return (1);
+
+	return (0);
+}
