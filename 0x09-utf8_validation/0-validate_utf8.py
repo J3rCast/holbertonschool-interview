329ddc03368ecdf9a6b5eff43abe75c3614443f2
J3rCast <jerson.castro.r@gmail.com>
2023-02-18 11:43:28 -0500

    task corrected

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
 """
-Defines a method that determines if a given data set
-represents valid UTF-8 encoding
+validate UTF8 module
 """
 
 
 def validUTF8(data):
-    for i in data:
-        if len(chr(i).encode('utf-8')) != 1:
+    """validUTF8
+    Determines if a given data set represents a valid UTF-8 encoding.
+    Args:
+        data (list): The data set can contain multiple characters
+    Returns:
+        bool: True if data is a valid UTF-8 encoding,
+              else return False.
+    """
+    valid = 0
+    for value in data:
+        byte = value & 255
+        if valid:
+            if byte >> 6 != 2:
+                return False
+            valid -= 1
+            continue
+        while (1 << abs(7 - valid)) & byte:
+            valid += 1
+        if valid == 1 or valid > 4:
             return False
-    return True
+        valid = max(valid - 1, 0)
+    return valid == 0
